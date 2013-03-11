dbf5f6fda6bfbcf2b867a975229028b8934b5f3d
Guillermo Indalecio <guillermobox@gmail.com>
2013-03-11 17:44:51 +0100

    Added a new mesh file parser

--- meshinfo.py
+++ meshinfo.py
@@ -25,6 +25,42 @@ def metric__determinant(v1, v2, v3, v4):
 
     return abs(temp1 - temp2)
 
+def parsefile_msh(filename):
+    fh = open(filename, 'r')
+    assert fh.readline().strip()=='$MeshFormat'
+    assert fh.readline().startswith('2.2')
+    assert fh.readline().strip()=='$EndMeshFormat'
+    assert fh.readline().strip()=='$Nodes'
+    nnodes = int(fh.readline())
+    nodes = [None]*nnodes
+    i=0
+    while i < nnodes:
+        line = fh.readline()
+        (ind,x,y,z) = line.split()
+        position = numpy.array(map(float, (x,y,z)))
+        nodes[i] = position
+        i += 1
+
+    assert fh.readline().strip()=='$EndNodes'
+    assert fh.readline().strip()=='$Elements'
+
+    total_nelem = int(fh.readline())
+    tetra_nelem = 0
+    elems = []
+    i_elem = 0
+    while i_elem < total_nelem:
+        i_elem += 1
+        line = fh.readline()
+        (i, sides, tmp1, tmp2, tmp3, line) = tuple(line.split(None, 5))
+        if int(sides)==4:
+            tetra_nelem += 1
+            vert = map(int, line.split())
+            elems.append( (nodes[v-1] for v in vert) )
+
+    assert fh.readline().strip()=='$EndElements'
+    fh.close()
+    return elems
+
 def parsefile_red(filename):
     fh = open(filename, 'r')
     fh.readline()
@@ -32,7 +68,7 @@ def parsefile_red(filename):
 
     nodes = []
     i=0
-    while i+2 <= nnodes:
+    while i+1 < nnodes:
         line = fh.readline()
         (i, ifront, icont, line) = tuple( line.split(None, 3) )
         (x, y, z, line) = tuple( line.split(None,3) )
@@ -50,17 +86,21 @@ def parsefile_red(filename):
     return coords
 
 def process(filename, metrics):
-    print 'Processing:', filename
-    
+    print
+    print filename.center(80,'=')
+    print
 
     if filename.endswith('.red.dat'):
-        results = {}
         coords = parsefile_red(filename)
-        for (k,f) in metrics.iteritems():
-            results[k] = map(lambda c:f(*c), coords)
+    elif filename.endswith('.msh'):
+        coords = parsefile_msh(filename)
     else:
-        print 'Unrecognied file format'
+        print 'Ups! I cant recognize that format!'
         return
+
+    results = {}
+    for (k,f) in metrics.iteritems():
+        results[k] = map(lambda c:f(*c), coords)
 
     print '%15s %13s %13s %13s %13s'%('metric','mean', 'std', 'min', 'max')
     for (key, array) in results.iteritems():
@@ -70,7 +110,6 @@ def process(filename, metrics):
 def main():
     if len(sys.argv)<2:
         print 'Usage: {0} <meshfile> [<meshfile> ...]'.format(sys.argv[0])
-        print 'Try using the file ending with .red.dat as an input file'
         print
         exit(1)
 
