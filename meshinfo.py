e7173e8874f43afe8c04c41f2baccbdcca94ce9f
Guillermo Indalecio <guillermobox@gmail.com>
2013-03-11 13:24:04 +0100

    Rewriting
    Adaptation of the code to be able to implement my ideas of plugable
    metrics and mesh file formaters.

--- meshinfo.py
+++ meshinfo.py
@@ -1,20 +1,19 @@
-# Busco un nodo en particular de la red que entra al simulador
+# !/usr/bin/env python
+#
+# Mesh analyzer
+# Analice the mesh, apply metrics and print a summary
+#
+# Guillermo Indalecio Fernandez, March-2013
+
 import sys
+import numpy
 
-if len(sys.argv)!=2:
-    print 'Uso: {0} <whatever.1.red.dat>'.format(sys.argv[0])
-    exit(0)
+def metric__determinant(v1, v2, v3, v4):
+    'Determinant of the element'
 
-f = open(sys.argv[1], 'r')
-f.readline() # linea vacia!
-nnodos = int(f.readline())
-
-def determinante(coordenadas):
-    def diff(x): return x[1]-x[0]
-    (v1,v2,v3,v4) = coordenadas[:]
-    r1 = map(diff, zip(v1,v2))
-    r2 = map(diff, zip(v1,v3))
-    r3 = map(diff, zip(v1,v4))
+    r1 = v2 - v1
+    r2 = v3 - v1
+    r3 = v4 - v1
 
     temp1 = r1[0]*r2[1]*r3[2] +\
             r1[2]*r2[0]*r3[1] +\
@@ -26,42 +25,63 @@ def determinante(coordenadas):
 
     return abs(temp1 - temp2)
 
-def condicion(x, y, z, xn, yn, zn):
-    if sum(map( abs, [x-xn, y-yn, z-zn])) < 1e-3:
-        return True
+def parsefile_red(filename):
+    fh = open(filename, 'r')
+    fh.readline()
+    nnodes = int(fh.readline())
 
-print 'Leo nodos'
-coordenadas = []
-nodos = []
-while 1:
-    line = f.readline()
-    (i, ifront, icont, line) = tuple( line.split(None, 3) )
-    (x, y, z, line) = tuple( line.split(None,3) )
-    (x,y,z) = map(float, (x,y,z))
-    i = int(i)
-    nodos.append( (x,y,z) )
+    nodes = []
+    i=0
+    while i+2 <= nnodes:
+        line = fh.readline()
+        (i, ifront, icont, line) = tuple( line.split(None, 3) )
+        (x, y, z, line) = tuple( line.split(None,3) )
+        position = numpy.array( map(float, (x,y,z)) )
+        i = int(i)
+        nodes.append( position )
 
-    if nnodos==i+1: break
+    nelems = int(fh.readline())
+    coords = [None]*nelems
+    for (i, line) in enumerate(fh.readlines()):
+        vert = map(int, line.split())
+        coords[i] = (nodes[v] for v in vert)
 
-print 'Leidos', nnodos, 'nodos'
+    fh.close()
+    return coords
 
-print 'Leo elementos'
-dets = []
-nelems = int(f.readline())
-for line in f.readlines():
-    vertices = map(int,line.split())
-    mis_vertices = tuple( nodos[i] for i in vertices )
-    det = determinante(mis_vertices)
-    dets.append(det)
-    if det<1e-20:
-        print 'Encontrado determinante<1e-20:',det
-        print 'Vertices:', vertices
-        print 'Coordenadas:'
-        for co in mis_vertices:
-            print '%13.6e %13.6e %13.6e'%co
+def process(filename, metrics):
+    print 'Processing:', filename
+    
+
+    if filename.endswith('.red.dat'):
+        results = {}
+        coords = parsefile_red(filename)
+        for (k,f) in metrics.iteritems():
+            results[k] = map(lambda c:f(*c), coords)
+    else:
+        print 'Unrecognied file format'
+        return
+
+    for (key, array) in results.iteritems():
+        array = numpy.array(array)
+        print key, len(array), array.mean(), array.std(), array.min(), array.max()
+
+def main():
+    if len(sys.argv)<2:
+        print 'Usage: {0} <meshfile> [<meshfile> ...]'.format(sys.argv[0])
+        print 'Try using the file ending with .red.dat as an input file'
         print
+        exit(1)
 
-print 'Leidos',nelems,'elementos'
-f = open('determinantes.txt', 'w')
-for d in dets: f.write('%13.6e\n'%d)
-f.close()
+    metrics = {}
+    for name in globals().keys():
+        if name.startswith('metric__'):
+            print 'Found metric (%s): %s'%(name[8:], globals()[name].__doc__)
+            metrics[name[8:]] = globals()[name] 
+
+    print
+    for filename in sys.argv[1:]: 
+        process(filename, metrics)
+
+if __name__=='__main__':
+    main()
